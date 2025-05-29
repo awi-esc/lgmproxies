d2b232d14b2a4136be41643202d72825a553e4f6
perrette <mahe.perrette@pik-potsdam.de>
2025-05-29 13:32:23 +0200

    add some context + pypi packages

--- lgmproxies/datasets/tierney.py
+++ lgmproxies/datasets/tierney.py
@@ -5,4 +5,12 @@ import urllib.parse
 from lgmproxies.logs import logger
 from lgmproxies.config import get_datapath
 
-TIERNEY_REPOS = ["jesstierney/BAYSPLINE", "jesstierney/BAYMAG", "jesstierney/BAYSPAR", "brews/baysparpy", "brews/d18oc_sst", "brews/bayfox"]
+TIERNEY_REPOS = [
+    "jesstierney/BAYSPLINE",
+    "jesstierney/BAYMAG",
+    "jesstierney/BAYSPAR",
+    "brews/baysparpy", # python port of jesstierney/BAYSPAR
+    # "brews/bayfox", # foram calibration: pypi
+    # "brews/erebusfall", # d18O correction adapted from Tierney al 2017 https://doi.org/10.1130/G39457.1: pypi
+    "brews/d18oc_sst", # d18O correction from Malevich et al 2019 https://doi.org/10.1029/2019PA003576
+    ]
